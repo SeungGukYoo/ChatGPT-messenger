c92393b857b37e8719ac1f67e69e901039802bf3
SeungGukYoo <dbtmd878@gmail.com>
2023-02-21 16:14:14 +0900

    next-auth를 사용하여 로그인하였을 때 유저 프로필 출력

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,11 @@
+"use client";
 import React from "react";
 import NewChat from "./NewChat";
+import { useSession, signOut } from "next-auth/react";
 
 function SideBar() {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -10,6 +14,16 @@ function SideBar() {
           <div>{/* model selection*/}</div>
           {/* map through the chart rows*/}
         </div>
+      </div>
+      <div>
+        {session && (
+          <img
+            onClick={() => signOut()}
+            src={session.user?.image!}
+            alt="Profile image"
+            className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          />
+        )}
       </div>
     </div>
   );
