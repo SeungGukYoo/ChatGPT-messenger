2817c8947fea79a885da380f601ffa0e38e5ea60
SeungGukYoo <dbtmd878@gmail.com>
2023-02-21 13:50:18 +0900

    next-auth를 통해 현재 로그인 상태별로 로그인 페이지 및 메인 페이지를 보여주는 컴포넌트

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,34 @@
 import "../styles/globals.css";
+import SideBar from "../components/SideBar";
+import { SessionProvider } from "../components/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../pages/auth/[...nextauth]";
+import Login from "../components/Login";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html>
       <head />
       <body>
-        <div>
-          {/* slider */}
-          {/*  clientProvider - notification*/}
-          <div>{children}</div>
-        </div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <div className="bg-[#202123] max-w-sx h-screen overflow-y-auto md:min-w-[20rem]">
+                <SideBar />
+              </div>
+
+              {/*  clientProvider - notification*/}
+              <div className="bg-[#353541] flex-1">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
