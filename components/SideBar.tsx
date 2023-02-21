68e9de8006ed71697b9ee48e2b65ed7dfe2ede49
SeungGukYoo <dbtmd878@gmail.com>
2023-02-21 13:44:33 +0900

    메인화면 사이드바 스타일링

---
+++ components/SideBar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NewChat from "./NewChat";
+
+function SideBar() {
+  return (
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
+        <div>
+          <NewChat />
+          <div>{/* model selection*/}</div>
+          {/* map through the chart rows*/}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
