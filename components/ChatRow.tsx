5d9ffeae1b50c2ab1f53913cd8c2e8105e7b517e
SeungGukYoo <dbtmd878@gmail.com>
2023-02-22 16:39:02 +0900

    작은 따옴표에서 큰 따옴표로 수정

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
-import { ChatBubbleLeftIcon, TrashIcon } from '@heroicons/react/24/outline';
-import { usePathname, useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
-import { useCollection } from 'react-firebase-hooks/firestore';
-import { collection, deleteDoc, doc, orderBy, query } from '@firebase/firestore';
-import { db } from '../firebase';
+import Link from "next/link";
+import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { usePathname, useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, deleteDoc, doc } from "@firebase/firestore";
+import { db } from "../firebase";
 
 type Props = {
   id: string;
@@ -17,12 +17,12 @@ function ChatRow({ id }: Props) {
   const { data: session } = useSession();
   const [active, setActive] = useState(false);
 
-  const [messages] = useCollection(collection(db, 'users', session?.user?.email!, 'chats', id, 'messages'));
+  const [messages] = useCollection(collection(db, "users", session?.user?.email!, "chats", id, "messages"));
 
   const removeChat = async () => {
-    await deleteDoc(doc(db, 'users', session?.user?.email!, 'chats', id));
+    await deleteDoc(doc(db, "users", session?.user?.email!, "chats", id));
 
-    router.replace('/');
+    router.replace("/");
   };
 
   useEffect(() => {
@@ -30,10 +30,10 @@ function ChatRow({ id }: Props) {
     setActive(pathname.includes(id));
   }, [pathname]);
   return (
-    <Link href={`/chat/${id}`} className={`chatRow justify-center ${active && 'bg-gray-700/50'}`}>
-      <ChatBubbleLeftIcon className='h-5 w-5' />
-      <p className='flex-1 hidden md:inline-flex'>{messages?.docs[messages?.docs.length - 1]?.data().text || 'New Chat'}</p>
-      <TrashIcon onClick={removeChat} className='h-5 w-5 text-gray-700 hover:text-red-700' />
+    <Link href={`/chat/${id}`} className={`chatRow justify-center ${active && "bg-gray-700/50"}`}>
+      <ChatBubbleLeftIcon className="h-5 w-5" />
+      <p className="flex-1 hidden md:inline-flex">{messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}</p>
+      <TrashIcon onClick={removeChat} className="h-5 w-5 text-gray-700 hover:text-red-700" />
     </Link>
   );
 }
