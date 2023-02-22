87f2521212982a454d6439e78b0441baf9486420
SeungGukYoo <dbtmd878@gmail.com>
2023-02-22 17:19:02 +0900

    메세지 출력 및 스타일링

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+"use client";
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import React from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import Message from "./Message";
 
 type Props = {
   chatId: string;
 };
 function Chat({ chatId }: Props) {
-  return <div className='flex-1'>chat</div>;
+  const { data: session } = useSession();
+  const [message] = useCollection(session && query(collection(db, "users", session?.user?.email!, "chats", chatId, "messages"), orderBy("createdAt", "asc")));
+
+  return (
+    <div className="flex-1 overflow-y-auto">
+      {message?.docs.map((message) => (
+        <Message key={message.id} message={message.data()} />
+      ))}
+    </div>
+  );
 }
 
 export default Chat;
