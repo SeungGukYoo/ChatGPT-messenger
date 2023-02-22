0ab80035200e9f80c9bbf38d97bea3e78e25e888
SeungGukYoo <dbtmd878@gmail.com>
2023-02-22 14:44:57 +0900

    firebase 메세지 저장 및 react-hot-toast 기능 추가

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,31 +1,75 @@
-'use client';
+"use client";
 
-import { PaperAirplaneIcon } from '@heroicons/react/24/solid';
-import { useSession } from 'next-auth/react';
-import React, { useState } from 'react';
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { db } from "../firebase";
 
 type Props = {
   chatId: string;
 };
 
 function ChatInput({ chatId }: Props) {
-  const [prompt, setPrompt] = useState('');
+  const [prompt, setPrompt] = useState("");
   const { data: session } = useSession();
+  const model = "text-davinci";
+  const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!prompt) return;
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar: session?.user?.image! || `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    };
+
+    const notification = toast.loading("ChatGPT is thinking");
+    await addDoc(collection(db, "users", session?.user?.email!, "chats", chatId, "messages"), message);
+
+    // toast notification
+
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    }).then(() => {
+      toast.success("ChatGPT has responded", {
+        id: notification,
+      });
+
+      // Toast notification to say successful
+    });
+  };
   return (
-    <div className='bg-gray-700/50 text-gray-400 rounded-lg text-sm '>
-      <form className='p-5 space-x-5 flex'>
+    <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm ">
+      <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
         <input
-          className='bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300 '
+          className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300 "
           disabled={!session}
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
-          type='text'
-          name=''
-          id=''
-          placeholder='Type your message here'
+          type="text"
+          name=""
+          id=""
+          placeholder="Type your message here"
         />
-        <button type='submit' disabled={!session || !prompt} className='bg-[#11a37f] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed'>
-          <PaperAirplaneIcon className='w-4 h-4 -rotate-45' />
+        <button type="submit" disabled={!session || !prompt} className="bg-[#11a37f] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed">
+          <PaperAirplaneIcon className="w-4 h-4 -rotate-45" />
         </button>
       </form>
 
