c1109672750da263b58c8214aed7bf5b6fcd34b6
SeungGukYoo <dbtmd878@gmail.com>
2023-02-22 14:04:53 +0900

    채팅컴포넌트, 메세지입력 컴폰넌트 추가 및 스타일링

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React from 'react';
+import Chat from '../../../components/Chat';
+import ChatInput from '../../../components/ChatInput';
 
-function ChatPage() {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+function ChatPage({ params: { id } }: Props) {
   return (
-    <div>
+    <div className='flex flex-col h-screen overflow-hidden'>
       <h1>chat page</h1>
+      <Chat chatId={id} />
+      <ChatInput chatId={id} />
     </div>
   );
 }
