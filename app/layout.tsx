580f65f472ce8b1f3c85dcf5b33d8859c0f3c45a
SeungGukYoo <dbtmd878@gmail.com>
2023-02-23 14:50:00 +0900

    사이드바를 감싸는 div 스타일 오타로 인한 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import '../styles/globals.css';
-import SideBar from '../components/SideBar';
-import { SessionProvider } from '../components/SessionProvider';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../pages/api/auth/[...nextauth]';
-import Login from '../components/Login';
-import ClientProvider from '../components/ClientProvider';
+import "../styles/globals.css";
+import SideBar from "../components/SideBar";
+import { SessionProvider } from "../components/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import Login from "../components/Login";
+import ClientProvider from "../components/ClientProvider";
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
@@ -17,7 +17,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <Login />
           ) : (
             <div className="flex">
-              <div className="bg-[#202123] max-w-sx h-screen overflow-y-auto md:min-w-[20rem]">
+              <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
                 <SideBar />
               </div>
               <ClientProvider />
