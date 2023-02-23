af863f11f58655d12a4eae9991654e5f591b3f81
SeungGukYoo <dbtmd878@gmail.com>
2023-02-23 13:39:18 +0900

    대화가 없을 때 채팅 문구 추가

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 import { collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import React from "react";
@@ -14,7 +15,13 @@ function Chat({ chatId }: Props) {
   const [message] = useCollection(session && query(collection(db, "users", session?.user?.email!, "chats", chatId, "messages"), orderBy("createdAt", "asc")));
 
   return (
-    <div className="flex-1 overflow-y-auto">
+    <div className="flex-1 overflow-y-auto overflow-x-hidden">
+      {message?.empty && (
+        <>
+          <p className="mt-10 text-center text-white">Type a prompt in below to get started!</p>
+          <ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 text-white animate-bounce" />
+        </>
+      )}
       {message?.docs.map((message) => (
         <Message key={message.id} message={message.data()} />
       ))}
